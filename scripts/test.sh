769dcdc271817cd80fc02e149341d146135cd0ef
Andrew Rynhard <andrew@andrewrynhard.com>
2017-06-05 10:48:50 -0700

    Use gometalinter and fix linting errors (#22)

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,13 +8,11 @@ GOFILES=$(find . -type f -name '*.go' -not -path "./vendor/*" -not -path "./api/
 COVERAGE_REPORT=coverage.txt
 PROFILE=profile.out
 
+echo "Running tests"
 if [[ -f ${COVERAGE_REPORT} ]]; then
   rm ${COVERAGE_REPORT}
 fi
-
 touch ${COVERAGE_REPORT}
-
-echo "Running tests"
 for package in ${GOPACKAGES[@]}; do
   go test -v -race -coverprofile=${PROFILE} -covermode=atomic $package
   if [ -f ${PROFILE} ]; then
@@ -23,17 +21,8 @@ for package in ${GOPACKAGES[@]}; do
   fi
 done
 
-echo "Vetting packages"
-go vet ${GOPACKAGES}
-if [ $? -eq 1 ]; then
-  exit 1
-fi
-
 echo "Linting packages"
-golint -set_exit_status ${GOPACKAGES}
-if [ $? -eq 1 ]; then
-  exit 1
-fi
+gometalinter.v1 --vendor --exclude=api/* --disable=gas --disable=gotype --sort=linter --deadline=240s ./...
 
 echo "Formatting go files"
 if [ ! -z "$(gofmt -l -s ${GOFILES})" ]; then
